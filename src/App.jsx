ca750346be99c2c701ee1c732d559614dcd87b6a
Anastasiia Volvis <stacyvolvis@gmail.com>
2024-03-24 21:58:24 +0100

    Add basic language switch functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import i18next from "i18next";
+import { useTranslation } from "react-i18next";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { t } = useTranslation();
 
   return (
     <>
@@ -17,19 +19,22 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <p className="read-the-docs">{t("Welcome")}</p>
+      <button
+        disabled={i18next.language === "en"}
+        onClick={() => i18next.changeLanguage("en")}
+      >
+        EN
+      </button>
+      <button
+        disabled={i18next.language === "de"}
+        onClick={() => i18next.changeLanguage("de")}
+      >
+        DE
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
